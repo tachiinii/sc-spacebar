6ac37a815d7c8c27746bc2b7be246381e703248d
Colen Wilson <colen.wilson@domail.maricopa.edu>
2019-03-17 12:25:44 -0700

    Add logger service for example.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -43,9 +44,11 @@ class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
    */
-  public function toggleArticleHeart($slug) {
+  public function toggleArticleHeart($slug, LoggerInterface $logger) {
 
     // TODO: add toggle to database
+
+    $logger->info("Article is being hearted.");
 
     return $this->json(['hearts' => rand(5, 100)]);
   }
