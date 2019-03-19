08e74bdc77d3f632608db94dae089f188dd4368e
Colen Wilson <colen.wilson@domail.maricopa.edu>
2019-03-18 22:40:58 -0700

    Set apcu for prod and create markdown service.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,10 +9,9 @@
 namespace App\Controller;
 
 
-use Michelf\MarkdownInterface;
+use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,7 +27,7 @@ class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache) {
+  public function show($slug, MarkdownHelper $markdownHelper) {
 
     $comments = [
       'I ate a normal rock once. It did NOT taste like bacon!',
@@ -55,12 +54,7 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
-    $item = $cache->getItem('markdown_'.md5($articleContent));
-    if (!$item->isHit()) {
-      $item->set($markdown->transform($articleContent));
-      $cache->save($item);
-    }
-    $articleContent = $item->get();
+    $articleContent = $markdownHelper->parse($articleContent);
 
     return $this->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
