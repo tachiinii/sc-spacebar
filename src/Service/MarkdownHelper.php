c72f2f3d29bb1cc04916341e22ed41313f9306a9
Colen Wilson <colen.wilson@domail.maricopa.edu>
2019-03-19 00:14:35 -0700

    Separate markdown function and add logger.

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -10,6 +10,7 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
@@ -18,14 +19,20 @@ class MarkdownHelper
 
   private $markdown;
 
-  public function __construct(AdapterInterface $cache, MarkdownInterface $markdown)
+  private $logger;
+
+  public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $logger)
   {
     $this->cache = $cache;
     $this->markdown = $markdown;
+    $this->logger = $logger;
   }
 
   public function parse(string $source): string
   {
+    if (stripos($source, 'bacon') !== false) {
+      $this->logger->info('They are talking about bacon again!');
+    }
     $item = $this->cache->getItem('markdown_'.md5($source));
     if (!$item->isHit()) {
       $item->set($this->markdown->transform($source));
