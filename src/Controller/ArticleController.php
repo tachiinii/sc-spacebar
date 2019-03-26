cf568a3ec4adb6d536e3dff3af051a003362ceab
Colen Wilson <colen.wilson@domail.maricopa.edu>
2019-03-25 23:10:54 -0700

    Move cache settings to parameters.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -27,8 +27,7 @@ class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug, MarkdownHelper $markdownHelper) {
-
+  public function show($slug, MarkdownHelper $markdownHelper, bool $isDebug) {
     $comments = [
       'I ate a normal rock once. It did NOT taste like bacon!',
       'Woohoo! I\'m going on an all-asteroid diet!',
